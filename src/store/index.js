4ba050f517dfb1bde24c21ec4b403849dee31817
Peter <psquicciarini@redventures.com>
2018-01-07 19:40:15 -0500

    Update store

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,12 @@ import main from './main';
 import user from './user';
 import client from './client';
 import shell from './shell';
+import dnsuser from './dnsuser';
 
 export default {
   main,
   user,
   client,
   shell,
+  dnsuser,
 };
